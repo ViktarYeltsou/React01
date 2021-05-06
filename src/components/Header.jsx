30599d0e61de7081168d99444f65b063c4e910be
ViktarYeltsou <victorius76@gmail.com>
2021-05-06 19:56:53 +0300

    second

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://www.dsgroup.by/bitrix/templates/aspro-corporation/images/logo.png'/>
+        </header>
+    );
+}
+export default Header;
