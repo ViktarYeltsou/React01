53f73c509ad86e425382fa2d1b4099103be51839
ViktarYeltsou <victorius76@gmail.com>
2021-05-21 00:42:41 +0300

    34

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -5,8 +5,9 @@ const ProfileInfo = () => {
         <div className={s.info}>
             <div>
                 <img
-                    alt=''
-                    src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'/>
+                    src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'
+                    alt=''/>
+
             </div>
             <div className={s.descriptionBlock}>
                 ava + description
